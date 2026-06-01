3cfd696759996b39ddb34576c32331c572b580ae
BenjaminBaredes <bbaredes@gmail.com>
2026-06-01 10:43:02 -0300

    clase 2

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider col){
+        
+        if(col.gameObject.CompareTag("Coleccionable")){
+            Debug.Log("Hubo colision con Coleccionable");
+            Destroy(col.gameObject);
+        }
+   }
+}
